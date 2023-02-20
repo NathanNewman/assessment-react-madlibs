ba143eb0e9916a0b806441f17e7f1196b54bf4cd
Nathan Newman <lordnate.moose@gmail.com>
2023-02-20 15:47:30 -0600

    added MadlibsForm

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+/** Simple app that just shows the Madlibs game. */
+
+function App() {
+  return (
+    <div className="App">
+      <Madlibs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Madlibs.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+const Madlibs = () => {
+    const [ madlib, setMadlib ] = useState();
+
+}
